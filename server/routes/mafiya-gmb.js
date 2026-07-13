1e256cff2c111662d869fda1cfd8685e71c7a6ca
BM-ACADEMY <teamkamarweb@gmail.com>
2026-07-13 13:30:57 +0530

    update

--- server/routes/mafiya-gmb.js
+++ server/routes/mafiya-gmb.js
@@ -41,6 +41,8 @@ router.get('/auth/:clientId', async (req, res) => {
       state: clientId.toString(), // Pass clientId through OAuth state
     });
 
+    
+
     res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Expires', '0');

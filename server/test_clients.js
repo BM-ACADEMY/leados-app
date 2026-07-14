50c5d65699bc224e71de0e94fe040abe82d27b40
BM-ACADEMY <teamkamarweb@gmail.com>
2026-07-14 15:55:48 +0530

    test file deleted

--- server/test_clients.js
+++
@@ -1,3 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({ user: 'leados_user', host: 'leados-api.abmgroups.org', database: 'leados_db', password: 'LeadOS_DB@2026', port: 5432 });
-pool.query("SELECT id, phone_number_id, wa_access_token FROM clients").then(res => { console.log(res.rows); process.exit(0); });
--- server/test_db.js
+++
@@ -1,8 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  user: 'leados_user', host: 'leados-api.abmgroups.org', database: 'leados_db', password: 'LeadOS_DB@2026', port: 5432
-});
-pool.query("SELECT id, direction, content, msg_type, status FROM messages ORDER BY id DESC LIMIT 5").then(res => {
-  console.log(res.rows);
-  process.exit(0);
-});
